f804bed072701c33426fbf54565575ef482c4295
OmidTarkhaneh <otarkhaneh@mun.ca>
2021-06-16 17:38:09 -0230

    Add Atomospheric.py to generate a plot

---
+++ Atomospheric.py
@@ -0,0 +1,32 @@
+import pymagicc
+from pymagicc import MAGICC6
+import pandas as pd
+
+from matplotlib import pyplot as plt
+
+
+data_dfrm=pd.read_csv('totalrcp_dataset.csv')
+
+datafrm_filtered = data_dfrm[(data_dfrm.region == 'World') & (data_dfrm.variable == 'Atmospheric Concentrations|CO2')]
+
+datafrm_filtered= datafrm_filtered.drop(['climate_model', 'model', 'region', 'todo', 'unit', 'variable'], axis=1)
+newdatafrm= datafrm_filtered.T.reset_index().drop(0)
+
+# newdatafrm.reindex()
+newdatafrm.columns = [ 'year','RCP26', 'RCP45', 'RCP60', 'RCP85']
+newdatafrm['year']=pd.to_datetime(newdatafrm['year'], errors = 'coerce')
+df=newdatafrm
+
+plt.figure(figsize=(12, 8))
+plt.plot(df['year'], df['RCP26'], label='RCP26')
+plt.plot(df['year'], df['RCP45'], label='RCP45')
+plt.plot(df['year'], df['RCP60'], label='RCP60')
+plt.plot(df['year'], df['RCP85'], label='RCP85')
+plt.legend(title='Scenario list')
+plt.xlabel('year')
+plt.ylabel('Atmospheric Concentrations|CO2 (1760-2100)')
+plt.grid()
+plt.title("Atmospheric Concentrations|CO2 Projection")
+
+plt.savefig("Atmospheric.png", dpi=150)
+
