591f5defc8d99a8a717163005e28a4baf4891ef3
OmidTarkhaneh <otarkhaneh@mun.ca>
2021-06-20 13:49:17 -0230

    Atomospheric.py modified

--- Atomospheric.py
+++ Atomospheric.py
@@ -4,29 +4,33 @@ import pandas as pd
 
 from matplotlib import pyplot as plt
 
+def main():
 
-data_dfrm=pd.read_csv('totalrcp_dataset.csv')
+    data_dfrm=pd.read_csv('totalrcp_dataset.csv')
 
-datafrm_filtered = data_dfrm[(data_dfrm.region == 'World') & (data_dfrm.variable == 'Atmospheric Concentrations|CO2')]
+    datafrm_filtered = data_dfrm[(data_dfrm.region == 'World') & (data_dfrm.variable == 'Atmospheric Concentrations|CO2')]
 
-datafrm_filtered= datafrm_filtered.drop(['climate_model', 'model', 'region', 'todo', 'unit', 'variable'], axis=1)
-newdatafrm= datafrm_filtered.T.reset_index().drop(0)
+    datafrm_filtered= datafrm_filtered.drop(['climate_model', 'model', 'region', 'todo', 'unit', 'variable'], axis=1)
+    newdatafrm= datafrm_filtered.T.reset_index().drop(0)
 
-# newdatafrm.reindex()
-newdatafrm.columns = [ 'year','RCP26', 'RCP45', 'RCP60', 'RCP85']
-newdatafrm['year']=pd.to_datetime(newdatafrm['year'], errors = 'coerce')
-df=newdatafrm
+    # newdatafrm.reindex()
+    newdatafrm.columns = [ 'year','RCP26', 'RCP45', 'RCP60', 'RCP85']
+    newdatafrm['year']=pd.to_datetime(newdatafrm['year'], errors = 'coerce')
+    df=newdatafrm
 
-plt.figure(figsize=(12, 8))
-plt.plot(df['year'], df['RCP26'], label='RCP26')
-plt.plot(df['year'], df['RCP45'], label='RCP45')
-plt.plot(df['year'], df['RCP60'], label='RCP60')
-plt.plot(df['year'], df['RCP85'], label='RCP85')
-plt.legend(title='Scenario list')
-plt.xlabel('year')
-plt.ylabel('Atmospheric Concentrations|CO2 (1760-2100)')
-plt.grid()
-plt.title("Atmospheric Concentrations|CO2 Projection")
+    plt.figure(figsize=(12, 8))
+    plt.plot(df['year'], df['RCP26'], label='RCP26')
+    plt.plot(df['year'], df['RCP45'], label='RCP45')
+    plt.plot(df['year'], df['RCP60'], label='RCP60')
+    plt.plot(df['year'], df['RCP85'], label='RCP85')
+    plt.legend(title='Scenario list')
+    plt.xlabel('year')
+    plt.ylabel('Atmospheric Concentrations|CO2 (1760-2100)')
+    plt.grid()
+    plt.title("Atmospheric Concentrations|CO2 Projection")
 
-plt.savefig("Atmospheric.png", dpi=150)
+    plt.savefig("Atmospheric.png", dpi=150)
 
+
+if __name__=='__main__':
+    main()
